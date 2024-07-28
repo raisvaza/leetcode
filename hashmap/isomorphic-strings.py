8133a427105e3f58815a8a66ce2f28d1a67f49e5
Vaza <raisvaza@gmail.com>
2024-07-28 11:23:15 +0700

    hashmap: isomorphic-strings

---
+++ hashmap/isomorphic-strings.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def isIsomorphic(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        
+        length = len(s)
+        if length != len(t):
+            return False
+
+        indexS = [-1] * 200
+        indexT = [-1] * 200
+
+        for i in range(length):
+            if indexS[ord(s[i])] != indexT[ord(t[i])]:
+                return False
+
+            indexS[ord(s[i])] = i
+            indexT[ord(t[i])] = i
+        return True
